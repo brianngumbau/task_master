e19201c2d004dced1d1d015399dd7d3a6e26605e
brianngumbau <brianngumbau9075@gmail.com>
2024-06-20 14:33:43 +0300

    update

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
+import pg from "pg";
 
 
 const app = express();
 const port = 4000;
 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "taskmaster",
+    password: "918190",
+    port: 5432,
+});
+db.connect();
+
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let tasks = [
-{
+/*{
     id: 1,
     title: "Code reviews",
     category: "Work",
@@ -20,31 +31,43 @@ let tasks = [
     id: 2,
     title: "Online course progress",
     category: "Educational",
-    description: "Track and complete the next set of modules in the 'JavaScript Algorithms and Data Structures'",
+    description: "Track and complete the next set of modules in the 'JavaScript Algorithms and Data Structures.",
     duedate: "21/6/2024",
-},
+}, */
 ];
 
 let lastId = 2;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+
 
 
 // get all tasks
-app.get("/tasks", (req, res) => {
-    res.json(tasks);
+app.get("/tasks", async (req, res) => {
+    try {
+        const result = await db.query("SELECT * FROM tasks ORDER BY id ASC");
+        const tasks = result.rows;
+        console.log(tasks);
+        res.json(tasks);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 // get a specific task by id
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
     const id = parseInt(req.params.id);
-    const task = tasks.find((task) => task.id === id);
-    res.json(task);
+    //const task = tasks.find((task) => task.id === id);
+    try {
+        const result = await db.query("SELECT * FROM tasks WHERE id = ($1)", [id]);
+        const task = result.rows;
+        console.log(task);
+        res.json(task);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 //posting a new task
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
     const newId = lastId += 1;
     const newTask = {
         id: newId,
@@ -53,8 +76,16 @@ app.post("/tasks", (req, res) => {
         description: req.body.description,
         category: req.body.category,
     };
-    tasks.push(newTask);
-    res.status(201).json(newTask)
+    //tasks.push(newTask);
+    try {
+        await db.query("INSERT INTO tasks (title, duedate, category, description) VALUES ($1, $2, $3, $4)", 
+            [req.body.title, req.body.duedate, req.body.category, req.body.description]);
+        
+            res.status(201).json(newTask);
+    } catch (error) {
+        console.log(error);
+    }
+    
 });
 
 //patching a task
@@ -76,9 +107,15 @@ app.patch("/tasks/:id", (req, res) => {
 
 //deleting specific task using task id
 
-app.delete("/tasks/:id", (req, res) => {
+app.delete("/tasks/:id", async (req, res) => {
     const id = parseInt(req.params.id);
-    const searchId = tasks.findIndex((task) => task.id === id);
+    try {
+        await db.query("DELETE FROM tasks WHERE id = ($1)", [id]);
+        res.sendStatus(200)
+    } catch (error) {
+        console.log(error);
+    }
+    /*const searchId = tasks.findIndex((task) => task.id === id);
     if (searchId > -1) {
         tasks.splice(searchId, 1);
         res.sendStatus(200);
@@ -86,7 +123,7 @@ app.delete("/tasks/:id", (req, res) => {
         res
         .status(404)
         .json({ error: "task not found"});
-    }
+    }*/
 });
 
 
