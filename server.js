c73ae1837c31170e2f0430d440c89cc073e51a60
brianngumbau <brianngumbau9075@gmail.com>
2024-06-25 10:07:01 +0300

    update

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import env from "dotenv";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 const API_URL = "http://localhost:4000";
+//const saltRounds = 10;
+//env.config();
 
 
 
 app.use(express.static("public"));
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "taskamaster",
+    password: "918190",
+    port: 5432
+});
+db.connect();
 
 //main page
 app.get("/", async (req, res) => {
@@ -54,7 +65,53 @@ app.get("/register", (req, res) => {
     res.render("signup.ejs");
 });
 
+app.post("/register", async (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
 
+    try {
+        const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+
+        if (checkResult.rows.length > 0) {
+            res.send("Email already exists!Try logging in")
+        } else {
+            const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2", 
+                [email, password]
+            );
+            console.log(result);
+            res.render("index.ejs");
+        }
+    } catch (error) {
+        console.log(error)
+    }
+
+});
+
+app.post("/login", async (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+
+    try {
+        const checkResult = await db.query("SELECT * FROM users WHERE email = ($1)", [email]);
+
+        if (checkResult.rows.length > 0) {
+            console.log(checkResult.rows);
+
+            const user = checkResult.rows[0];
+            const savedPassword = user.password;
+
+            if (password === savedPassword){
+                res.render("index.ejs");
+            } else {
+                res.send("Incorrect password");
+            }
+        } else {
+            res.send("User not found")
+        }
+    } catch (error) {
+        console.log(error)
+    }
+});
 
 
 // creating new task
